1223984e3d6277e581608b1466c05d0a56d99968
Dozana <levan.soroznishvili@gmail.com>
2021-12-03 16:48:28 +0400

    Create a route and a view file

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,14 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Simple Note
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('about', function () {
+    return view('about');
 });
