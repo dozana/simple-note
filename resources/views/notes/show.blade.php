34d9b58ac1c47cbf7b971196438cde91dd2bf953
Dozana <levan.soroznishvili@gmail.com>
2021-12-03 21:21:14 +0400

    Layout, partials and @yield

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,42 +1,12 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Simple Notes</title>
-    <style>
-        body {
-            padding-top: 70px;
-        }
-    </style>
-</head>
-<body>
+@extends('layouts.app')
 
-<nav class="navbar navbar-expand navbar-dark bg-dark fixed-top" aria-label="Second navbar example">
-    <div class="container">
-        <a class="navbar-brand" href="/">Simple Note</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+@section('title')
+    Note - {{ $note->title }}
+@endsection
 
-        <div class="collapse navbar-collapse" id="navbarsExample02">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="https://github.com/dozana/simple-note" target="_blank">Github</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
+@section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-lg-12 col-md-12">
 
             <div class="card mb-3">
@@ -71,8 +41,4 @@
         </div>
     </div>
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-</body>
-</html>
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark fixed-top" aria-label="Second navbar example">
+    <div class="container">
+        <a class="navbar-brand" href="/notes">Simple Note</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample02">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/notes">Notes</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/dozana/simple-note" target="_blank">Github</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
