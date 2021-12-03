a2fe33f7dd0f74c81779d5f6ef40652747abb4bc
Dozana <levan.soroznishvili@gmail.com>
2021-12-03 19:51:23 +0400

    Views bootstrap styling

--- database/seeds/NotesSeeder.php
+++ database/seeds/NotesSeeder.php
@@ -11,6 +11,6 @@ class NotesSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Note::class, 10)->create();
+        factory(App\Note::class, 5)->create();
     }
 }
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,19 +1,78 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Notes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Simple Notes</title>
+    <style>
+        body {
+            padding-top: 70px;
+        }
+    </style>
 </head>
 <body>
 
-<h1>Simple Notes</h1>
-<ul>
-    @foreach($notes as $note)
-        <li>{{ $note->title }}</li>
-    @endforeach
-</ul>
+<nav class="navbar navbar-expand navbar-dark bg-dark fixed-top" aria-label="Second navbar example">
+    <div class="container">
+        <a class="navbar-brand" href="/">Simple Note</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample02">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/dozana/simple-note" target="_blank">Github</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-12 col-md-12">
+
+            <div class="card">
+                <div class="card-body p-0">
+                    <table class="table table-hover mb-0">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th class="text-center">Completed</th>
+                                <th>Created At</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($notes as $note)
+                            <tr>
+                                <td>{{ $note->title }}</td>
+                                <td class="text-center">{{ $note->completed }}</td>
+                                <td>{{ $note->created_at }}</td>
+                                <td class="text-center">
+                                    <form action="">
+                                        <button class="btn btn-primary btn-sm">View</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
